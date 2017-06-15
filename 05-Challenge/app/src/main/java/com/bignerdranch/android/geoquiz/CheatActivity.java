cc09f3a1fddf6bd03101d9a9d1a77f04189c655d
ChloeLiang <nliangxin@gmail.com>
2017-06-15 21:17:47 +0800

    Chapter 5 challenge

---
+++ 05-Challenge/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -0,0 +1,73 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE =
+            "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN =
+            "com.bignerdranch.android.geoquiz.answer_shown";
+    private static final String KEY_INDEX = "cheat_index";
+
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
+
+    public static Intent newIntent(Context packageContext, boolean isAnswerTrue) {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, isAnswerTrue);
+        return intent;
+    }
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+
+        if (savedInstanceState != null) {
+            String message = savedInstanceState.getString(KEY_INDEX, "");
+            mAnswerTextView.setText(message);
+            setAnswerShownResult(true);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_INDEX, mAnswerTextView.getText().toString());
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+}
