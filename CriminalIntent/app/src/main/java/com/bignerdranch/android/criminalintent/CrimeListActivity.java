5ffeb7b2107c14b3e62328b0bcfc151d416cd6d2
ChloeLiang <nliangxin@gmail.com>
2017-06-18 14:13:46 +0800

    8. Displaying Lists with RecyclerView

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,10 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
