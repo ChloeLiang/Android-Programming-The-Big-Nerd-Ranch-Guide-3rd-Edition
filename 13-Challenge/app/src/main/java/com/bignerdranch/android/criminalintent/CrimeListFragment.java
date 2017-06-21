3d406ccc8cc62f0fadd7ab0ef62c16561bb0d53a
ChloeLiang <nliangxin@gmail.com>
2017-06-21 21:47:28 +0800

    Chapter 13 challenge: Plural String Resources

--- 13-Challenge/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ 13-Challenge/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -92,7 +92,8 @@ public class CrimeListFragment extends Fragment {
     private void updateSubtitle() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         int crimeCount = crimeLab.getCrimes().size();
-        String subtitle = getString(R.string.subtitle_format, crimeCount);
+        String subtitle = getResources()
+                .getQuantityString(R.plurals.subtitle_plural, crimeCount, crimeCount);
 
         if (!mSubtitleVisible) {
             subtitle = null;
