da8e5c8ab92c8b4a6d4e6b7488d722ab45236828
ChloeLiang <nliangxin@gmail.com>
2017-06-24 14:27:36 +0800

    Check for responding activities

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -126,6 +127,12 @@ public class CrimeFragment extends Fragment {
 
         if (mCrime.getSuspect() != null) {
             mSuspectButton.setText(mCrime.getSuspect());
+        }
+
+        PackageManager packageManager = getActivity().getPackageManager();
+        if (packageManager.resolveActivity(pickContact,
+                PackageManager.MATCH_DEFAULT_ONLY) == null) {
+            mSuspectButton.setEnabled(false);
         }
 
         return v;
