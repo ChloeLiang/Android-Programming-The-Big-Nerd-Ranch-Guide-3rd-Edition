1311cf38ba72b4af86572035adc433a9d420b33a
ChloeLiang <nliangxin@gmail.com>
2017-06-30 20:43:13 +0800

    19 challenge starting point

---
+++ 19-Challenge/app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+
+public class PictureUtils {
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay()
+                .getSize(size);
+        return getScaledBitmap(path, size.x, size.y);
+    }
+
+    public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            float heightScale = srcHeight / destHeight;
+            float widthScale = srcWidth / destWidth;
+
+            inSampleSize = Math.round(heightScale > widthScale ? heightScale : widthScale);
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        return BitmapFactory.decodeFile(path, options);
+    }
+}
