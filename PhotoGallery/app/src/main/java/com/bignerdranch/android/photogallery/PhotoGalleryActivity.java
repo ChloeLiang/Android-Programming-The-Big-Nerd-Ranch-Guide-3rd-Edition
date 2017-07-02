909106c03b902213c9d267bbc4379402f1f7fc13
ChloeLiang <nliangxin@gmail.com>
2017-07-02 22:19:27 +0800

    25. HTTP and Background Tasks - Create PhotoGallery

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+
+}
---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PhotoGalleryFragment extends Fragment {
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return v;
+    }
+}
