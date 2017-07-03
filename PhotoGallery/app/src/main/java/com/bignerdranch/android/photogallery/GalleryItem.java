4bd75e1f7cce76f2524dcce181d484b21b766a7c
ChloeLiang <nliangxin@gmail.com>
2017-07-03 22:22:31 +0800

    AsyncTask, JSON

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -0,0 +1,36 @@
+package com.bignerdranch.android.photogallery;
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+}
