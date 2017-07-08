bd3fa3cf748c88c28adb36898e6642f845964e99
ChloeLiang <nliangxin@gmail.com>
2017-07-08 10:35:02 +0800

    31. Custom Views and Touch Events

---
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DragAndDrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drag_and_draw);
+    }
+}
