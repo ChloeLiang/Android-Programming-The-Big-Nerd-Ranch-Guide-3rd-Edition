7bf95951a420ddb9b2379ea2547e6693071ed1f9
ChloeLiang <nliangxin@gmail.com>
2017-07-08 20:57:16 +0800

    Check Play Services

--- Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
@@ -1,11 +1,37 @@
 package com.bignerdranch.android.locatr;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class LocatrActivity extends SingleFragmentActivity {
+    private static final int REQUEST_ERROR = 0;
 
     @Override
     protected Fragment createFragment() {
         return LocatrFragment.newInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = apiAvailability
+                    .getErrorDialog(this, errorCode, REQUEST_ERROR,
+                            new DialogInterface.OnCancelListener() {
+                                @Override
+                                public void onCancel(DialogInterface dialogInterface) {
+                                    finish();
+                                }
+                            });
+            errorDialog.show();
+        }
     }
 }
