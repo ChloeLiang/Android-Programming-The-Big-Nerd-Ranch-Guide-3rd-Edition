69f01c18c662f3e7caa159365a93f0263c6ffb3e
ChloeLiang <nliangxin@gmail.com>
2017-07-08 20:05:07 +0800

    33. Locations and Play Services
    - Create Locatr

---
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/GalleryItem.java
@@ -0,0 +1,55 @@
+package com.bignerdranch.android.locatr;
+
+import android.net.Uri;
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("https://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+}
---
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.locatr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocatrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locatr);
+    }
+}
