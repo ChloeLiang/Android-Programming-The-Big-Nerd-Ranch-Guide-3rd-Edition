f7c61cfe239758b2f5d8aef76005bce3434bc891
ChloeLiang <nliangxin@gmail.com>
2017-07-08 20:48:03 +0800

    Build out Locatr

--- Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrActivity.java
@@ -1,13 +1,11 @@
 package com.bignerdranch.android.locatr;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class LocatrActivity extends AppCompatActivity {
+public class LocatrActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_locatr);
+    protected Fragment createFragment() {
+        return LocatrFragment.newInstance();
     }
 }
---
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.locatr;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class LocatrFragment extends Fragment {
+    private ImageView mImageView;
+
+    public static LocatrFragment newInstance() {
+        return new LocatrFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_locatr, container, false);
+
+        mImageView = (ImageView) v.findViewById(R.id.image);
+
+        return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
+    }
+}
