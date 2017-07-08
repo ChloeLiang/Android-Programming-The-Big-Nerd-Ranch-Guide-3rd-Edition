1e7142f9f07f27fc5f9bcf3f07f59038673c7e9e
ChloeLiang <nliangxin@gmail.com>
2017-07-08 12:53:23 +0800

    Color evaluation

--- Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.sunset;
 
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +16,10 @@ public class SunsetFragment extends Fragment {
     private View mSunView;
     private View mSkyView;
 
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
+
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
     }
@@ -26,6 +32,11 @@ public class SunsetFragment extends Fragment {
         mSceneView = view;
         mSunView = view.findViewById(R.id.sun);
         mSkyView = view.findViewById(R.id.sky);
+
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
 
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +57,12 @@ public class SunsetFragment extends Fragment {
                 .setDuration(3000);
         heightAnimator.setInterpolator(new AccelerateInterpolator());
 
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3000);
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
         heightAnimator.start();
+        sunsetSkyAnimator.start();
     }
 }
