e7ed8f246f932abaafc29eb699dc251b25efe2fa
ChloeLiang <nliangxin@gmail.com>
2017-07-08 12:28:10 +0800

    Build the scene

--- Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetActivity.java
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetActivity.java
@@ -1,13 +1,11 @@
 package com.bignerdranch.android.sunset;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class SunsetActivity extends AppCompatActivity {
+public class SunsetActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sunset);
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
     }
 }
---
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.sunset;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+        return view;
+    }
+}
