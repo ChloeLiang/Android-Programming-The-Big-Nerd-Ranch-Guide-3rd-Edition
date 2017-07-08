c1f64b49413246cc890771e47135c1efd9785e40
ChloeLiang <nliangxin@gmail.com>
2017-07-08 12:48:31 +0800

    Simple property animation
    - Animate the sun down below the horizon

--- Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SunsetFragment extends Fragment {
+
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
 
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
@@ -16,6 +21,29 @@ public class SunsetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAnimation();
+            }
+        });
+
         return view;
+    }
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+
+        heightAnimator.start();
     }
 }
