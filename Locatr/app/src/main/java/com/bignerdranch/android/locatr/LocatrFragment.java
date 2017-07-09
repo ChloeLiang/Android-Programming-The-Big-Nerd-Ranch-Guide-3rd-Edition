61c8a34940e2a1f7454113e7e664390eb4f3f2c4
ChloeLiang <nliangxin@gmail.com>
2017-07-09 10:56:50 +0800

    Zoom map

--- Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
@@ -23,7 +23,13 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 import java.io.IOException;
 import java.util.List;
@@ -37,6 +43,7 @@ public class LocatrFragment extends SupportMapFragment {
     private static final int REQUEST_LOCATION_PERMISSIONS = 0;
 
     private GoogleApiClient mClient;
+    private GoogleMap mMap;
     private Bitmap mMapImage;
     private GalleryItem mMapItem;
     private Location mCurrentLocation;
@@ -64,6 +71,14 @@ public class LocatrFragment extends SupportMapFragment {
                     }
                 })
                 .build();
+
+        getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                updateUI();
+            }
+        });
     }
 
     @Override
@@ -139,6 +154,25 @@ public class LocatrFragment extends SupportMapFragment {
         return result == PackageManager.PERMISSION_GRANTED;
     }
 
+    private void updateUI() {
+        if (mMap == null || mMapImage == null) {
+            return;
+        }
+
+        LatLng itemPoint = new LatLng(mMapItem.getLat(), mMapItem.getLon());
+        LatLng myPoint = new LatLng(
+                mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+
+        LatLngBounds bounds = new LatLngBounds.Builder()
+                .include(itemPoint)
+                .include(myPoint)
+                .build();
+
+        int margin = getResources().getDimensionPixelSize(R.dimen.map_inset_margin);
+        CameraUpdate update = CameraUpdateFactory.newLatLngBounds(bounds, margin);
+        mMap.animateCamera(update);
+    }
+
     private class SearchTask extends AsyncTask<Location, Void, Void> {
         private GalleryItem mGalleryItem;
         private Bitmap mBitmap;
@@ -171,6 +205,7 @@ public class LocatrFragment extends SupportMapFragment {
             mMapImage = mBitmap;
             mMapItem = mGalleryItem;
             mCurrentLocation = mLocation;
+            updateUI();
         }
     }
 }
